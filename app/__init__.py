b7226f347267ae23aa86d98ac0cde9f796120982
Yu Fang <squarefk@gmail.com>
2020-03-21 21:01:46 -0400

    add index html

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import random
 import string
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/random')
